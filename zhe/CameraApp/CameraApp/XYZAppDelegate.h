f8ba1176c853998ea55cfbcbd67da7e69fa86176
Zhe Song <songzhe.ustc@gmail.com>
2014-03-14 12:52:07 -0700

    add Zhe's folder

---
+++ zhe/CameraApp/CameraApp/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  CameraApp
+//
+//  Created by zhe-mac on 14-3-13.
+//  Copyright (c) 2014年 zhe-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
